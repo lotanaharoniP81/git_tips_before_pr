9459cfa85b443d6e89791e9b9708a0dd193afb61
Lotan Aharoni <lotan.aharoni@perimeter81.com>
2023-05-04 10:27:59 +0300

    add a client

---
+++ client.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("client")
+	fmt.Println("in progress - client")
+	fmt.Println("another in progress - client")
+	fmt.Println("fix typo")
+}
