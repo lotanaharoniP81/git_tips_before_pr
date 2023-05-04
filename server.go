edff5e8d2c1cda8fab8f7bf6f6e297d970b87418
Lotan Aharoni <lotan.aharoni@perimeter81.com>
2023-05-04 10:24:16 +0300

    P81-111 - create a server

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Server to init server object
+type Server struct {
+	ID   string
+	Name string
+}
+
+func main() {
+	fmt.Println("add")
+	fmt.Println("some fix in the server")
+}
